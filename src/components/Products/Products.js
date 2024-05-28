555376665760b4b408597d0e3b8459dd5d2544a9
Piotr Wawrzyniak <hola.wawrzyniak@gmail.com>
2024-05-28 22:51:49 +0200

    Before dividing app into components

---
+++ src/components/Products/Products.js
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+import productsData from '../../data/products';
+import Product from '../Product/Product';
+
+const Products = () => {
+  const [products]  = useState(productsData);
+
+
+
+
+  return (
+    <section>
+      <ul>
+        {products.map((product) =>
+       (
+        <Product key={product.id} {...product}
+          /> ))}
+      </ul>
+    </section>
+  );
+};
+
+export default Products;
